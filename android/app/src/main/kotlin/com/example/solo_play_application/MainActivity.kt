ac83d5a915a02b49c0020e102569b620ead88e42
WantOscar <kimdw3824@gmail.com>
2024-11-27 21:28:32 +0900

    솔플 프로젝트 시작

---
+++ android/app/src/main/kotlin/com/example/solo_play_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.solo_play_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Container(color: Colors.grey),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
