3f9faf591e34030fb331f0e809c62f0614ee905f
WantOscar <kimdw3824@gmail.com>
2025-01-10 20:43:52 +0900

    내 개인 화면 ui 제작 중

--- lib/src/views/profile_view.dart
+++ lib/src/views/profile_view.dart
@@ -16,13 +16,14 @@ class ProfileView extends StatelessWidget {
           ),
           elevation: 0.0,
           title: const Text(
-            'wlsdk_b님의 프로필',
+            '나의 포스팅',
             style: TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.w600,
               color: Colors.black87,
             ),
           ),
+          actions: const [Icon(Icons.more_vert)],
         ),
       ),
       body: SingleChildScrollView(
@@ -31,6 +32,11 @@ class ProfileView extends StatelessWidget {
           children: [
             _header(),
             const SizedBox(height: 11),
+            _name(),
+            const SizedBox(height: 11),
+            const SizedBox(height: 11),
+            _profileEditBtn(),
+            const SizedBox(height: 11),
             _bestCourse(),
           ],
         ),
@@ -38,51 +44,49 @@ class ProfileView extends StatelessWidget {
     );
   }
 
+  /// 프로필 사진, 게시물, 팔로워, 팔로잉을 보여줌.
   Widget _header() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12.0),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Padding(
             padding: const EdgeInsets.only(right: 12.0),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(100),
               child: Container(
-                width: 68,
-                height: 68,
+                width: 72,
+                height: 72,
                 color: Colors.grey,
               ),
             ),
           ),
-          const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'wlsdk_b',
-                style: TextStyle(
-                  fontSize: 29,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black87,
-                ),
-              ),
-              Text(
-                '혼자놀기 만랩 유저 #00001',
-                style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.black87,
-                ),
-              ),
-            ],
+          const Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: Column(
+              children: [
+                Text('50', style: TextStyle(fontSize: 30)),
+                Text('게시물', style: TextStyle(fontSize: 15)),
+              ],
+            ),
           ),
-          const Spacer(),
-          const Text(
-            '4K',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.w600,
-              color: Colors.black87,
+          const Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: Column(
+              children: [
+                Text('50', style: TextStyle(fontSize: 30)),
+                Text('팔로워', style: TextStyle(fontSize: 15)),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: Column(
+              children: [
+                Text('50', style: TextStyle(fontSize: 30)),
+                Text('팔로잉', style: TextStyle(fontSize: 15)),
+              ],
             ),
           ),
         ],
@@ -90,6 +94,92 @@ class ProfileView extends StatelessWidget {
     );
   }
 
+  /// 사용자의 닉네임, 지역을 보여줌.
+  Widget _name() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              const Text(
+                '너구리',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black87,
+                ),
+              ),
+              const SizedBox(width: 10),
+              Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 12.0, vertical: 4.0),
+                  child: Text(
+                    '중구',
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: List.generate(
+              3,
+              (index) {
+                return const Row(
+                  children: [
+                    Text(
+                      '#혼자서 잘놀아요',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// 프로필을 편집창으로 가는 프로필 편집 버튼.
+  Widget _profileEditBtn() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: GestureDetector(
+        onTap: () {},
+        child: Container(
+          height: 30,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: const Text(
+            '프로필 편집',
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// 나의 코스 몰아보기 화면으로 넘어가는 버튼과 가장 추천을 많이 받은 코스들 1,2,3 순위를 보여줌.
   Widget _bestCourse() {
     return Padding(
       padding: const EdgeInsets.all(12.0),
@@ -101,21 +191,14 @@ class ProfileView extends StatelessWidget {
               const Text(
                 '가장 추천을 많이 받은 코스',
                 style: TextStyle(
-                  fontSize: 18,
+                  fontSize: 30,
                   fontWeight: FontWeight.w600,
                   color: Colors.black87,
                 ),
               ),
               GestureDetector(
                 onTap: () {},
-                child: const Text(
-                  '더보기',
-                  style: TextStyle(
-                    fontSize: 10,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.black87,
-                  ),
-                ),
+                child: const Icon(Icons.arrow_forward_ios),
               ),
             ],
           ),
--- lib/src/widgets/course_widget.dart
+++ lib/src/widgets/course_widget.dart
@@ -5,6 +5,8 @@ class CourseWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> items = List<String>.generate(3, (i) => "$i");
+
     return Column(
       children: [
         /// 코스 지도 영역
@@ -21,7 +23,7 @@ class CourseWidget extends StatelessWidget {
           child: Container(),
         ),
 
-        /// 코스 태그, 위치, 별점 영역
+        /// 코스 태그, 위치, 영역
         Container(
           width: double.infinity,
           decoration: const BoxDecoration(
@@ -34,116 +36,86 @@ class CourseWidget extends StatelessWidget {
           child: Padding(
             padding:
                 const EdgeInsets.symmetric(horizontal: 12.0, vertical: 4.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 /// 태그 영역
-                const Text(
-                  '#소품샵',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.black87,
-                  ),
-                ),
-                const SizedBox(height: 4),
-
-                /// 코스 위치 영역
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                const Column(
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          '연남동',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.black87,
-                          ),
-                        ),
-
-                        /// 별점 영역
-                        Row(
-                          children: [
-                            Row(
-                              children: List.generate(
-                                5,
-                                (index) => const Icon(
-                                  Icons.star,
-                                  size: 16,
-                                  color: Colors.amber,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(width: 4),
-                            const Text(
-                              '5',
-                              style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.black87,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+                    Text(
+                      '#소품샵',
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black87,
+                      ),
                     ),
-                    SizedBox(
-                      width: 150,
-                      height: 70,
-                      child: Stack(
-                        alignment: Alignment.centerLeft,
-                        children: [
-                          Positioned(
-                            right: 50,
-                            child: SizedBox(
-                              width: 50,
-                              height: 50,
-                              child: CircleAvatar(
-                                radius: 10,
-                                backgroundColor: Colors.grey.shade300,
-                              ),
-                            ),
-                          ),
-                          Positioned(
-                            right: 60,
-                            child: SizedBox(
-                              width: 50,
-                              height: 50,
-                              child: CircleAvatar(
-                                radius: 10,
-                                backgroundColor: Colors.grey.shade400,
-                              ),
-                            ),
-                          ),
-                          Positioned(
-                            right: 70,
-                            child: SizedBox(
-                              width: 50,
-                              height: 50,
-                              child: CircleAvatar(
-                                radius: 10,
-                                backgroundColor: Colors.grey.shade500,
-                              ),
-                            ),
-                          ),
-                          const Positioned(
-                            left: 110,
-                            child: Text(
-                              '+5',
-                              style: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.black87,
-                              ),
-                            ),
-                          ),
-                        ],
+                    Text(
+                      '연남동',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black87,
                       ),
                     ),
                   ],
+                ),
+                const SizedBox(height: 4),
+
+                /// 코스
+                SizedBox(
+                  width: 150,
+                  height: 70,
+                  child: Stack(
+                    alignment: Alignment.centerLeft,
+                    children: [
+                      Positioned(
+                        right: 20,
+                        child: SizedBox(
+                          width: 50,
+                          height: 50,
+                          child: CircleAvatar(
+                            radius: 10,
+                            backgroundColor: Colors.grey.shade300,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        right: 30,
+                        child: SizedBox(
+                          width: 50,
+                          height: 50,
+                          child: CircleAvatar(
+                            radius: 10,
+                            backgroundColor: Colors.grey.shade400,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        right: 40,
+                        child: SizedBox(
+                          width: 50,
+                          height: 50,
+                          child: CircleAvatar(
+                            radius: 10,
+                            backgroundColor: Colors.grey.shade500,
+                          ),
+                        ),
+                      ),
+                      const Positioned(
+                        left: 130,
+                        bottom: 13,
+                        child: Text(
+                          '+5',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black87,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
